4854b727d67c891facacf5dbb7f076cd81778ff6
Val-de-mar <vladimir.a.melnikov@phystech.edu>
2021-04-14 11:45:01 +0300

    review changes

--- contest6/taskB.cpp
+++ contest6/taskB.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
- 
-int bin_search(const std::vector<int>& vect, int size, int val) {
+
+int bin_search(const std::vector<int> &vect, int size, int val) {
     int l = 0, r = size;
-    while (l < r){
+    while (l < r) {
         if (vect[(l + r) / 2] <= val) {
             r = (l + r) / 2;
         } else {
@@ -12,44 +12,52 @@ int bin_search(const std::vector<int>& vect, int size, int val) {
     }
     return l;
 }
- 
+
 struct Node {
+    Node(int place, int val, Node *prev) : place(place), val(val), prev(prev) {}
+
+    Node() {}
+
+
     int place;
     int val;
-    Node* prev;
-    Node(int place, int val, Node* prev) : place(place), val(val), prev(prev) {}
-    Node(){}
+    Node *prev;
 };
- 
-int main() {
+
+void optimise() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
-    int n, sc, pl, size = 0;
- 
+}
+
+int main() {
+    optimise();
+    int n, scaner, current_place, size = 0;
+
     std::cin >> n;
-    std::vector<int> vec(n);
-    std::vector<Node*> vecn(n), all(n);
+    std::vector<int> vec_of_rising_vals(n);
+    std::vector<Node *> chain_vect(n), all(n);
     for (int i = 0; i < n; ++i) {
-        std::cin >> sc;
-        pl = bin_search(vec, size, sc);
-        vec[pl] = sc;
-        vecn[pl] = new Node(i + 1, sc, (pl > 0 ? vecn[pl - 1] : nullptr));
-        all[i] = vecn[pl];
-        if (pl == size) {
+        std::cin >> scaner;
+        current_place = bin_search(vec_of_rising_vals, size, scaner);
+        vec_of_rising_vals[current_place] = scaner;
+        chain_vect[current_place] = new Node(i + 1, scaner,
+                                             (current_place > 0 ? chain_vect[current_place - 1] : nullptr));
+        all[i] = chain_vect[current_place];
+        if (current_place == size) {
             ++size;
         }
     }
     std::cout << size << std::endl;
-    auto curr = vecn[size - 1];
+    auto curr = chain_vect[size - 1];
     for (int i = size; i > 0; --i) {
-        vec[i - 1] = curr -> place;
-        curr = curr -> prev;
+        vec_of_rising_vals[i - 1] = curr->place;
+        curr = curr->prev;
     }
     for (int i = 0; i < size; ++i) {
-        std::cout << vec[i] << " ";
+        std::cout << vec_of_rising_vals[i] << " ";
     }
-    for (auto & i : all) {
+    for (auto &i : all) {
         delete i;
     }
     return 0;
